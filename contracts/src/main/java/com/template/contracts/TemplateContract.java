58398cd9e7d9feb467ff42a6ca7c6e0d30514431
Iryna Tsimashenka <iryna.tsimashenka@r3.com>
2020-10-26 19:06:55 +0000

    Adding project files

---
+++ contracts/src/main/java/com/template/contracts/TemplateContract.java
@@ -0,0 +1,46 @@
+package com.template.contracts;
+
+import com.template.states.MedicalRecordsState;
+import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.CommandWithParties;
+import net.corda.core.contracts.Contract;
+import net.corda.core.transactions.LedgerTransaction;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
+import static net.corda.core.contracts.ContractsDSL.requireThat;
+
+// ************
+// * Contract *
+// ************
+public class TemplateContract implements Contract {
+    // This is used to identify our contract when building a transaction.
+    public static final String ID = "com.template.contracts.TemplateContract";
+
+    // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
+    // does not throw an exception.
+    @Override
+    public void verify(LedgerTransaction tx) {
+
+        /* We can use the requireSingleCommand function to extract command data from transaction.
+         * However, it is possible to have multiple commands in a signle transaction.*/
+
+        ArrayList<String> rejectCountries = new ArrayList<String>(Arrays.asList("RU"));
+
+        final CommandWithParties<Commands> command = requireSingleCommand(tx.getCommands(), Commands.class);
+        String requesterCountry = command.component2().get(0).getName().getCountry();
+        requireThat(require -> {
+            require.using("Initiator should be not from rejected countries.", !(rejectCountries.contains(requesterCountry)));
+            return null;
+        });
+    }
+
+    // Used to indicate the transaction's intent.
+    public interface Commands extends CommandData {
+        //In our hello-world app, We will only have one command.
+        class Create implements Commands {}
+        class Request implements Commands {}
+    }
+}
---
+++ workflows/src/test/java/com/template/FlowTests.java
@@ -0,0 +1,65 @@
+package com.template;
+
+import com.google.common.collect.ImmutableList;
+import com.template.flows.FillMedicalRecords;
+import com.template.flows.RequestPatientRecords;
+import com.template.flows.ResponderPatientRecords;
+import net.corda.core.concurrent.CordaFuture;
+import net.corda.core.identity.CordaX500Name;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.testing.node.MockNetwork;
+import net.corda.testing.node.MockNetworkParameters;
+import net.corda.testing.node.StartedMockNode;
+import net.corda.testing.node.TestCordapp;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+public class FlowTests {
+    private MockNetwork network;
+    private StartedMockNode a;
+    private StartedMockNode b;
+
+    @Before
+    public void setup() {
+        network = new MockNetwork(new MockNetworkParameters().withCordappsForAllNodes(ImmutableList.of(
+                TestCordapp.findCordapp("com.template.contracts"),
+                TestCordapp.findCordapp("com.template.flows"))));
+        a = network.createPartyNode(new CordaX500Name("StMarysHospital", "London", "GB"));
+        b = network.createPartyNode(new CordaX500Name("CharitéHospital", "Berlin", "DE"));
+        // For real nodes this happens automatically, but we have to manually register the flow for tests.
+        for (StartedMockNode node : ImmutableList.of(a, b)) {
+            node.registerInitiatedFlow(ResponderPatientRecords.class);
+        }
+        network.runNetwork();
+    }
+
+    @After
+    public void tearDown() {
+        network.stopNodes();
+    }
+
+    @Test
+    public void dummyTest2() throws ExecutionException, InterruptedException {
+        CordaFuture<SignedTransaction> future = a.startFlow(new FillMedicalRecords("Alexey", "data"));
+        network.runNetwork();
+        SignedTransaction ptx = future.get();
+        assert(ptx.getTx().getInputs().isEmpty());
+    }
+
+    @Test
+    public void dummyTest() throws ExecutionException, InterruptedException {
+        CordaFuture<SignedTransaction> future = a.startFlow(new FillMedicalRecords("Alexey", "data"));
+        network.runNetwork();
+        SignedTransaction ptx = future.get();
+
+        CordaFuture<SignedTransaction> future1 = b.startFlow(new RequestPatientRecords(a.getInfo().getLegalIdentities().get(0),"Alexey"));
+        network.runNetwork();
+        SignedTransaction ptx1 = future1.get();
+        assert(!ptx1.getTx().getOutputs().isEmpty());
+    }
+
+
+}
